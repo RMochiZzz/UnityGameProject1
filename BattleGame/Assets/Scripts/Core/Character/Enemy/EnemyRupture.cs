f5288b925798b28417c4486e137339c9c30c3721
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-16 13:08:05 +0900

    Edit : Edit the display of the rupture object to be gradient

--- BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyRupture.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyRupture.cs
@@ -5,18 +5,37 @@ namespace Core.Character.Enemy
 {
     public class EnemyRupture : MonoBehaviour
     {
+        private float gradationTime = 1;
+
         public void Rupture(GameObject ruptureObj)
         {
             Vector3 spornPosition = gameObject.transform.position;
 
             GameObject obj = Instantiate(ruptureObj, spornPosition, Quaternion.identity);
-            StartCoroutine(DestroyAfterOneFrame(obj));
+            StartCoroutine(Gradation(obj));
         }
 
-        private IEnumerator DestroyAfterOneFrame(GameObject obj)
+        private void ObjDestroy(GameObject obj)
         {
-            yield return new WaitForSeconds(1);
             Destroy(obj);
+        }
+
+        private IEnumerator Gradation(GameObject obj)
+        {
+            float t = 0f;
+            Color startColor = Color.clear;
+            Color endColor = Color.white;
+
+            SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+
+            while (t < 1f)
+            {
+                t += Time.unscaledDeltaTime / gradationTime;
+                spriteRenderer.color = Color.Lerp(startColor, endColor, t);
+                yield return null;
+            }
+
+            ObjDestroy(obj);
         }
     }
 }

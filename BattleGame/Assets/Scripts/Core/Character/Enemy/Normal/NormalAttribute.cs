6890b29a282fc8c78fc8e1bbfea38b264e0bf5b5
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-20 13:25:26 +0900

    Edit : Implement property

--- BattleGame/Assets/Scripts/Core/Character/Enemy/Normal/NormalAttribute.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/Normal/NormalAttribute.cs
@@ -5,8 +5,11 @@ namespace Core.Character.Enemy.Normal
     public class NormalAttribute : MonoBehaviour
     {
         [SerializeField] private float enemySpeed;
-        [SerializeField] private int stamina;
+        [SerializeField] private int enemyStamina;
         [SerializeField] private float spawnInterbal;
 
+        public float EnemySpeed { get => enemySpeed; set => enemySpeed = value; }
+        public int EnemyStamina { get => enemyStamina; set => enemyStamina = value; }
+        public int SpawnInterbal { get => spawnInterbal; set => spawnInterbal = value; }
     }
 }

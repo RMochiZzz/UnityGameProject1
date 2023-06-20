2eb5cd33de96821282adc0a1a302d062dba58402
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-20 13:02:45 +0900

    Create : Grouprush-type Enemy Information Scripts

---
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/GroupRush/GroupRushAttribute.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Core.Character.Enemy.GroupRush
+{
+    public class GroupRushAttribute : MonoBehaviour
+    {
+        [SerializeField] private float enemySpeed;
+        [SerializeField] private int stamina;
+        [SerializeField] private float spawnInterbal;
+
+    }
+}

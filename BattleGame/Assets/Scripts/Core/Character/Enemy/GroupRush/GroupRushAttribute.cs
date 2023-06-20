cebd8766c882e29b71ee3d0593c5bb5ee68eba7c
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-20 13:24:55 +0900

    Edit : Implement property

--- BattleGame/Assets/Scripts/Core/Character/Enemy/GroupRush/GroupRushAttribute.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/GroupRush/GroupRushAttribute.cs
@@ -1,3 +1,4 @@
+using SceneManagement.UI;
 using UnityEngine;
 
 namespace Core.Character.Enemy.GroupRush
@@ -5,8 +6,13 @@ namespace Core.Character.Enemy.GroupRush
     public class GroupRushAttribute : MonoBehaviour
     {
         [SerializeField] private float enemySpeed;
-        [SerializeField] private int stamina;
+        [SerializeField] private int enemyStamina;
         [SerializeField] private float spawnInterbal;
+
+        public float EnemySpeed { get => enemySpeed; set => enemySpeed = value; }
+        public int EnemyStamina { get => enemyStamina; set => enemyStamina = value; }
+        public int SpawnInterbal { get => spawnInterbal; set => spawnInterbal = value; }
+
 
     }
 }

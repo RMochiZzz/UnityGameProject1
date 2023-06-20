57640ba82072c7855c8d369c033595e9e9a0db20
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-20 13:25:48 +0900

    Edit : Implement property

--- BattleGame/Assets/Scripts/Core/Character/Enemy/Parasitoid/ParasitoidAttribute.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/Parasitoid/ParasitoidAttribute.cs
@@ -5,8 +5,11 @@ namespace Core.Character.Enemy.Parasitoid
     public class ParasitoidAttribute : MonoBehaviour
     {
         [SerializeField] private float enemySpeed;
-        [SerializeField] private int stamina;
+        [SerializeField] private int enemyStamina;
         [SerializeField] private float spawnInterbal;
 
+        public float EnemySpeed { get => enemySpeed; set => enemySpeed = value; }
+        public int EnemyStamina { get => enemyStamina; set => enemyStamina = value; }
+        public int SpawnInterbal { get => spawnInterbal; set => spawnInterbal = value; }
     }
 }

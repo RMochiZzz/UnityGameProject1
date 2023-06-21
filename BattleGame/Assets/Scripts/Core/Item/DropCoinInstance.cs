576b61ffe58a014b3f9008515ec3034f6932c2af
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-21 14:10:50 +0900

    Edit : Edit to store generated objects in the object from for organizing

--- BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
+++ BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
@@ -6,11 +6,13 @@ namespace Core.Item
     public class DropCoinInstance : MonoBehaviour
     {
         private EnemyInstanceStatus enemyInstanceStatus;
+        private GameObject container;
         public void Drop(GameObject dropPrefab, Transform dropPoint)
         {
             enemyInstanceStatus = GameObject.Find("EnemyManager").GetComponent<EnemyInstanceStatus>();
+            container = GameObject.Find("Coins");
 
-            Instantiate(dropPrefab, dropPoint.position, dropPoint.rotation);
+            Instantiate(dropPrefab, dropPoint.position, dropPoint.rotation, container.transform);
             enemyInstanceStatus.DropCoinInstanceCounter++;
         }
     }

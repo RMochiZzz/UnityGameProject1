0b500bd14e3f0f53b86841088916b9f27e3f0cf9
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-26 12:22:10 +0900

    Edit : Clear unused variables

--- BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyAttribute.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyAttribute.cs
@@ -4,12 +4,10 @@ namespace Core.Character.Enemy
 {
     public class EnemyAttribute
     {
-        private int instanceMax;
         private int instanceCounter;
         private int dropCoinInstanceCounter;
         private int destroyCounter;
 
-        public int InstanceMax { get => instanceMax; set => instanceMax = value; }
         public int InstanceCounter { get => instanceCounter; set => instanceCounter = value; }
         public int DestroyCounter { get => destroyCounter; set => destroyCounter = value; }
         public int DropCoinInstanceCounter { get => dropCoinInstanceCounter; set => dropCoinInstanceCounter = value; }

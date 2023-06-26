416b387b02f03dcc14d753ecd5514bc1ca7d5fc0
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-26 10:24:40 +0900

    Edit : Edit because there was an error with no reference to the game object that would be the parent object of the generated object.

--- BattleGame/Assets/Scripts/Core/Item/EventPublisher/DropCoinInstance.cs
+++ BattleGame/Assets/Scripts/Core/Item/EventPublisher/DropCoinInstance.cs
@@ -4,12 +4,11 @@ namespace Core.Item.EventPublisher
 {
     public class DropCoinInstance : MonoBehaviour
     {
-        private GameObject container;
 
         public void HandleEvent(GameObject dropPrefab, Vector3 dropPoint)
         {
 
-            Instantiate(dropPrefab, dropPoint, Quaternion.identity, container.transform);
+            Instantiate(dropPrefab, dropPoint, Quaternion.identity);
             
         }
     }

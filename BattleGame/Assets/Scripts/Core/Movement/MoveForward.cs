0b30d569a72d3ae795bc5bc5a7e6932311349464
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-26 16:44:38 +0900

    Edit : Fix variable references

--- BattleGame/Assets/Scripts/Core/Movement/MoveForward.cs
+++ BattleGame/Assets/Scripts/Core/Movement/MoveForward.cs
@@ -1,20 +1,30 @@
+using Core.Character.Enemy;
 using UnityEngine;
 
 namespace Core.Movement
 {
     public class MoveForward : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed;
+        private EnemyStatus enemyStatus;
 
+        private void Awake()
+        {
+            Reference();
+        }
         private void Update()
         {
 
             Vector2 forwardDirection = transform.up;
 
-            Vector2 move = forwardDirection * moveSpeed * Time.deltaTime;
+            Vector2 move = forwardDirection * enemyStatus.EnemySpeed * Time.deltaTime;
 
             transform.position += new Vector3(move.x, move.y, 0f);
 
+        }
+
+        private void Reference() 
+        {
+            enemyStatus = GetComponentInParent<EnemyStatus>();
         }
     }
 }

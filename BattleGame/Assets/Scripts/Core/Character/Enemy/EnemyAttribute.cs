d58dd4df8c082f8a74e5e7b2d8e547dd956f8f5e
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-26 10:37:16 +0900

    Edit : Destruction counters shifted to Enemy Attributes

--- BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyAttribute.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyAttribute.cs
@@ -8,10 +8,13 @@ namespace Core.Character.Enemy
         private int instanceMax;
         private int instanceCounter;
         private int dropCoinInstanceCounter;
+        private int destroyCounter;
 
         public Transform Container { get => container; set => container = value; }
         public int InstanceMax { get => instanceMax; set => instanceMax = value; }
         public int InstanceCounter { get => instanceCounter; set => instanceCounter = value; }
         public int DropCoinInstanceCounter { get => dropCoinInstanceCounter; set => dropCoinInstanceCounter = value; }
+        public int DestroyCounter { get => destroyCounter; set => destroyCounter = value; }
+
     }
 }

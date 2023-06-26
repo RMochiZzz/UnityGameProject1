8bd275a8b5d28323043f6a526317f93f66d3e893
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-26 12:45:38 +0900

    Edit : Clear variables that are no longer needed

--- BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyStatus.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/EnemyStatus.cs
@@ -6,11 +6,11 @@ namespace Core.Character.Enemy
     {
         [SerializeField] private float enemySpeed;
         [SerializeField] private int enemyStamina;
-        private int hitCounter;
+
         
         public float EnemySpeed { get => enemySpeed; set => enemySpeed = value; }
         public int EnemyStamina { get => enemyStamina; set => enemyStamina = value; }
-        public int HitCounter { get => hitCounter; set => hitCounter = value; }
+
 
     }
 }

adc24faf4e1728379ef8ecec443527438e7016af
Ryo Mochizuki <full.m.eclipse@gmail.com>
2023-06-27 16:42:56 +0900

    Edit : Added process to increment counter through interface

--- BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
+++ BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
@@ -1,3 +1,5 @@
+using Core.Character.Item.ValueManipulator;
+using Interface.Interfaces;
 using UnityEngine;
 
 namespace Core.Item
@@ -5,10 +7,13 @@ namespace Core.Item
     public class DropCoinInstance : MonoBehaviour
     {
         private GameObject container;
+
+        private IIncrement dropInstanceCounterIncrement;
         public void Starter(GameObject dropPrefab, Vector3 dropPoint)
         {
             GetReference();
             Drop(dropPrefab, dropPoint);
+            CounterIncrement();
         }
         private void Drop(GameObject dropPrefab, Vector3 dropPoint)
         {
@@ -17,9 +22,15 @@ namespace Core.Item
             
         }
 
+        private void CounterIncrement()
+        {
+            dropInstanceCounterIncrement.Increment();
+        }
+
         private void GetReference() 
         {
             container = GameObject.Find("Coins");
+            dropInstanceCounterIncrement = new DropInstanceCounterIncrement();
         }
         
     }

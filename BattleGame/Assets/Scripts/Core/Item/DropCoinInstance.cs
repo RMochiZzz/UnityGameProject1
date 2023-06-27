7b66d2da895be39e299f71e6138f308296092530
Ryo Mochizuki <full.m.eclipse@gmail.com>
2023-06-27 16:19:57 +0900

    Edit : Adding a variable to be referenced

--- BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
+++ BattleGame/Assets/Scripts/Core/Item/DropCoinInstance.cs
@@ -4,14 +4,14 @@ namespace Core.Item
 {
     public class DropCoinInstance : MonoBehaviour
     {
-        public void Starter(GameObject dropPrefab, Vector3 dropPoint)
+        public void Starter(GameObject dropPrefab, Vector3 dropPoint, GameObject container)
         {
-            Drop(dropPrefab, dropPoint);
+            Drop(dropPrefab, dropPoint, container);
         }
-        public void Drop(GameObject dropPrefab, Vector3 dropPoint)
+        public void Drop(GameObject dropPrefab, Vector3 dropPoint, GameObject container)
         {
 
-            Instantiate(dropPrefab, dropPoint, Quaternion.identity);
+            Instantiate(dropPrefab, dropPoint, Quaternion.identity, container.transform);
             
         }
     }

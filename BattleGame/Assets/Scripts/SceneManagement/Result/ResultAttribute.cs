0e76c6992ef6db5eec68c471d6cef8f8050c8987
ryo mochizuki <full.m.eclipse@gmail.com>
2023-06-29 11:02:58 +0900

    Update : Rename variable

--- BattleGame/Assets/Scripts/SceneManagement/Result/ResultAttribute.cs
+++ BattleGame/Assets/Scripts/SceneManagement/Result/ResultAttribute.cs
@@ -6,10 +6,10 @@ namespace SceneManagement.Result
         private int killCount;
         private int playerHealth;
 
-        public float Time { get => time; set => time = value; }
-        public int KillCount { get => killCount; set => killCount = value; }
+        public float RemainingTimeAtEnd { get => time; set => time = value; }
+        public int KillCountAtEnd { get => killCount; set => killCount = value; }
 
-        public int PlayerHealth { get => playerHealth; set => playerHealth = value; }
+        public int PlayerStaminaAtEnd { get => playerHealth; set => playerHealth = value; }
 
     }
 }

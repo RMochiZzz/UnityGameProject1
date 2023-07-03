84b33cb7737cacb5933ca6b6bfa22042a7529807
ryo mochizuki <full.m.eclipse@gmail.com>
2023-07-03 13:45:21 +0900

    Update : Changed method for exiting game for build

--- BattleGame/Assets/Scripts/SceneManagement/EndOfPlayback.cs
+++ BattleGame/Assets/Scripts/SceneManagement/EndOfPlayback.cs
@@ -6,9 +6,7 @@ namespace SceneManagement
     {
         public static void QuitGame()
         {
-            #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-            #endif
+            Application.Quit();
         }
     }
 

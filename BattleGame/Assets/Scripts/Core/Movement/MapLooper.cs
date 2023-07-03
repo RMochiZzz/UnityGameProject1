606bfebeb246b815eac201b97f563aac11e4d9ea
ryo mochizuki <full.m.eclipse@gmail.com>
2023-07-03 18:14:42 +0900

    Update : Map looping process implemented in a different way

---
+++ BattleGame/Assets/Scripts/Core/Movement/MapLooper.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Movement
+{
+    public class MapLooper : MonoBehaviour
+    {
+        [SerializeField] private float fieldWidth;
+        [SerializeField] private float fieldHeight;
+
+        private Vector3 playerPositionBefore;
+        private Vector3 playerPositionAfter;
+
+        private float leftBorder;
+        private float rightBorder;
+        private float upBorder;
+        private float downBorder;
+
+        private bool processObjectsThisFrame;
+        private bool isOutOfBounds;
+
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Update()
+        {
+            CheckBoundsAndSetProcessFlag();
+            ProcessObjects();
+        }
+
+        private void CheckBoundsAndSetProcessFlag()
+        {
+            playerPositionBefore = transform.position;
+            playerPositionAfter = transform.position;
+            CheckBorders();
+
+            if (isOutOfBounds)
+            {
+                processObjectsThisFrame = true;
+            }
+        }
+
+        private void ProcessObjects()
+        {
+            if (processObjectsThisFrame)
+            {
+                GameObject[] gameObjects = GetObjectsToProcess();
+
+                foreach (var item in gameObjects)
+                {
+                    Vector3 offset = item.transform.position - playerPositionBefore;
+                    Vector3 newPosition = playerPositionAfter + offset;
+
+                    item.transform.position = newPosition;
+                }
+
+                processObjectsThisFrame = false;
+            }
+        }
+
+        private GameObject[] GetObjectsToProcess()
+        {
+            List<GameObject> objectList = new List<GameObject>();
+
+            objectList.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+            objectList.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+            objectList.AddRange(GameObject.FindGameObjectsWithTag("Bullet"));
+            objectList.AddRange(GameObject.FindGameObjectsWithTag("DropCoin"));
+            objectList.AddRange(GameObject.FindGameObjectsWithTag("Rupture"));
+
+            return objectList.ToArray();
+        }
+
+        private void CheckBorders()
+        {
+            if (playerPositionAfter.x < leftBorder)
+            {
+                playerPositionAfter.x = rightBorder;
+                isOutOfBounds = true;
+            }
+
+            if (playerPositionAfter.x > rightBorder)
+            {
+                playerPositionAfter.x = leftBorder;
+                isOutOfBounds = true;
+            }
+
+            if (playerPositionAfter.y > upBorder)
+            {
+                playerPositionAfter.y = downBorder;
+                isOutOfBounds = true;
+            }
+
+            if (playerPositionAfter.y < downBorder)
+            {
+                playerPositionAfter.y = upBorder;
+                isOutOfBounds = true;
+            }
+        }
+
+        private void Init()
+        {
+            leftBorder = transform.position.x - fieldWidth / 2f;
+            rightBorder = transform.position.x + fieldWidth / 2f;
+            upBorder = transform.position.y + fieldHeight / 2f;
+            downBorder = transform.position.y - fieldHeight / 2f;
+
+            isOutOfBounds = false;
+        }
+    }
+}

df14dc34284b3c7274e88725258330d8dafe5f71
ryo mochizuki <full.m.eclipse@gmail.com>
2023-07-06 18:44:16 +0900

    Fix : Fixed to increment the destruction counter.

--- BattleGame/Assets/Scripts/Core/Character/Enemy/Battle/EnemyDestroyNoDrop.cs
+++ BattleGame/Assets/Scripts/Core/Character/Enemy/Battle/EnemyDestroyNoDrop.cs
@@ -1,17 +1,33 @@
+using Core.Character.Enemy.ValueManipulator;
+using Interface.Interfaces;
 using UnityEngine;
 
 namespace Core.Character.Enemy.Battle
 {
     public class EnemyDestroyNoDrop : MonoBehaviour
     {
+        private IIncrement enemyDestroyCounterIncrement;
+
         public void Starter()
         {
+            Reference();
             DestroyEnemy();
+            IncrementEnemyCounter();
         }
 
         private void DestroyEnemy()
         {
             Destroy(gameObject);
+        }
+
+        private void IncrementEnemyCounter()
+        {
+            enemyDestroyCounterIncrement.Increment();
+        }
+
+        private void Reference()
+        {
+            enemyDestroyCounterIncrement = new EnemyDestroyCounterIncrement();
         }
     }
 }
